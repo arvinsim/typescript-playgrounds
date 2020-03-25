627c369c9cdd58ec890b2b68c1468b0268c59756
Arthur Vincent Simon <developarvin@gmail.com>
2020-03-25 08:43:39 +0800

    Add more pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom'
-import './App.css';
+import { Route,Link, BrowserRouter } from 'react-router-dom'
 import {About} from "./pages/About"
+import {Functions} from "./pages/Functions"
+import {Generics} from "./pages/Generics"
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/functions">Functions</Link>
+                        </li>
+                        <li>
+                            <Link to="/generics">Generics</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
             <Route path="/about">
                 <About />
+            </Route>
+            <Route path="/functions">
+                <Functions />
+            </Route>
+            <Route path="/generics">
+                <Generics />
             </Route>
         </BrowserRouter>
     </div>
